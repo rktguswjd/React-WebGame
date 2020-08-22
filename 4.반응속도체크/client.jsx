0205843b1c0830d086818d5288971a6420b9c176
rktguswjd <010_cm@naver.com>
2020-08-22 16:50:16 +0900

    반응속도체크 class, React조건문, setTimeout 넣어 반응속도체크

---
+++ 4.반응속도체크/client.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+// require : 자바스크립트 자체가 지원하는 패키지 읽는 방법
+// import : ES6의 사양으로 새로운 패키지 읽는 방법
+import ReactDOM from "react-dom";
+import { hot } from "react-hot-loader/root";
+
+import ResponseCheck from "./ResponseCheck";
+const Hot = hot(ResponseCheck);
+
+ReactDOM.render(<Hot />, document.querySelector("#root"));
