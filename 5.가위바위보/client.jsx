318d23687f53d69ac4bd42c9f08aa88d2cb7c21f
rktguswjd <010_cm@naver.com>
2020-08-23 18:26:21 +0900

    리액트 setInterval과 라이프사이클, 가위바위보 Class

---
+++ 5.가위바위보/client.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+// require : 자바스크립트 자체가 지원하는 패키지 읽는 방법
+// import : ES6의 사양으로 새로운 패키지 읽는 방법
+import ReactDOM from "react-dom";
+import { hot } from "react-hot-loader/root";
+
+import RSPClass from "./RSPClass";
+const Hot = hot(RSPClass);
+
+ReactDOM.render(<Hot />, document.querySelector("#root"));
